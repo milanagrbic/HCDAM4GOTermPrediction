aee21fa3ddb453ce027483c65adb471dd64ed464
milana <milana23bl@gmail.com>
2025-05-27 22:35:18 +0200

    VNS

---
+++ VNS/runExample.bat
@@ -0,0 +1,3 @@
+rem  test <inputfile> <mink> <maxk> <itermax> <iterwochange> <prob> <seed> <lstype=2> <secondsmax> <reduce 1/0>
+
+test ../instance/dense/mapped/gd_50_5 1 100 1000 500 0.5 12345 2 14400 1
